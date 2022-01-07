8927ce09a44c22b29d4ed66d4f8f2efb0ec9db88
Alejandro Saez <alejandrosaezc@gmail.com>
2022-01-07 13:21:11 +0100

    Initial commit

---
+++ src/test/java/com/alsaecas/Tema101112_Ejercicios_Spring/Tema101112EjerciciosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alsaecas.Tema101112_Ejercicios_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tema101112EjerciciosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
